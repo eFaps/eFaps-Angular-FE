523de59832fdcf3542d46c06e0c3c9c4a7ae7f34
Jan Moxter <jan@moxter.net>
2023-06-05 20:56:12 -0500

    Resizable columns

--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
 import { TableRoutingModule } from './table-routing.module';
 import { TableModule as primeTableModule } from 'primeng/table';
-
+import { MultiSelectModule } from 'primeng/multiselect';
 @NgModule({
   declarations: [TableComponent],
-  imports: [CommonModule, TableRoutingModule, primeTableModule],
+  imports: [CommonModule, TableRoutingModule, primeTableModule, MultiSelectModule],
 })
 export class TableModule {}
