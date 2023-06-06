060c7dc3322d31a7046d62f6fe373622e8448f56
Jan Moxter <jan@moxter.net>
2023-06-05 22:22:02 -0500

    Correct url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   version: '1.0.0',
   apiHostname: undefined,
-  apiBaseUrl: '/servlet/rest',
+  apiBaseUrl: '.synercom.pe/servlet/rest',
   ssoUrl: 'https://sso.synercom.pe/auth',
   ssoRealm: 'demo',
   ssoClientId: 'localhost-test',
