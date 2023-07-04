0b7baab9ee1543958ba9573161145265f66d9490
Jan Moxter <jan@moxter.net>
2023-07-04 11:29:26 -0500

    Dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { GridsterModule } from 'angular-gridster2';
 import { CarouselModule } from 'primeng/carousel';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageComponent } from './page/page.component';
+
+
 @NgModule({
   declarations: [DashboardComponent, PageComponent],
-  imports: [CommonModule, DashboardRoutingModule, CarouselModule],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    CarouselModule,
+    GridsterModule,
+  ],
 })
 export class DashboardModule {}
