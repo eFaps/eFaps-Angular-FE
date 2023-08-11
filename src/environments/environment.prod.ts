59f2fb12c67e536b5f5546b1afef1328d457c373
Jan Moxter <jan@moxter.net>
2023-08-11 18:31:54 -0500

    Set env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   version: '1.0.0',
   apiHostname: undefined,
-  apiBaseUrl: '/servlet/rest',
+  apiBaseUrl: '/api',
   ssoUrl: 'https://sso.synercom.pe/auth',
   ssoRealm: 'demo',
   ssoClientId: 'localhost-test',
