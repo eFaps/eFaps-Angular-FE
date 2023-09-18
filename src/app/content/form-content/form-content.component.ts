680e0add3915be7d9c7353ef582127333e5c1814
Jan Moxter <jan@moxter.net>
2023-09-18 14:17:08 -0500

    Fomr content with menu

--- src/app/content/form-content/form-content.component.ts
+++ src/app/content/form-content/form-content.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
 import { combineLatest } from 'rxjs';
 import { Outline, Section } from 'src/app/model/content';
+import { MenuEntry } from 'src/app/model/menu';
 import { ContentService } from 'src/app/services/content.service';
 import { ValueService } from 'src/app/services/value.service';
+import { ModalContentComponent } from '../modal-content/modal-content.component';
 
 @Component({
   selector: 'app-form-content',
@@ -15,10 +19,12 @@ export class FormContentComponent implements OnInit {
   oid: string = 'none';
   outline: Outline | undefined;
   sections: Section[] = [];
-
+  menuItems: MenuItem[] = [];
+  
   constructor(
     valueService: ValueService,
     private route: ActivatedRoute,
+    private dialogService: DialogService,
     private contentService: ContentService
   ) {
     valueService.reset();
@@ -42,8 +48,55 @@ export class FormContentComponent implements OnInit {
         if ('sections' in outline) {
           this.outline = outline;
           this.sections = outline.sections;
+          this.menuItems = outline.menu
+          ? outline.menu.map((item) => this.getMenuItem(item))
+          : [];
         }
       },
     });
+  }
+
+  getMenuItem(item: MenuEntry): MenuItem {
+    return {
+      id: item.id,
+      label: item.label,
+      items:
+        item.children && item.children.length > 0
+          ? item.children.map((item) => this.getMenuItem(item))
+          : undefined,
+        command: this.evalAction(item),
+    };
+  }
+
+  evalAction(item: MenuEntry): ((event?: any) => void) | undefined {
+    switch (item.action.type) {
+      case 'FORM':
+        return (event) => {
+          this.formAction(item);
+        };
+    }
+    return undefined;
+  }
+
+  formAction(item: MenuEntry) {
+    if (item.action.modal) {
+      this.contentService.getContentWithCmd(this.oid!!, item.id).subscribe({
+        next: (outline) => {
+            const dialogRef = this.dialogService.open(ModalContentComponent, {
+              data: {
+                item,
+                outline,
+              },
+            });
+            dialogRef.onClose.subscribe({
+              next: (execResponse) => {
+                if (execResponse != null && execResponse.reload) {
+                  this.loadData();
+                }
+              },
+            });
+          },
+      });
+    }
   }
 }
