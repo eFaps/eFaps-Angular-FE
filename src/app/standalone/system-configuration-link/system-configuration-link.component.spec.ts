60a9da110cc1372155027123ff4bec7fbebe9b56
Jan Moxter <jan@moxter.net>
2023-12-27 22:37:14 -0500

    Update test

--- src/app/standalone/system-configuration-link/system-configuration-link.component.spec.ts
+++ src/app/standalone/system-configuration-link/system-configuration-link.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SystemConfigurationLinkComponent } from './system-configuration-link.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 describe('SystemConfigurationLinkComponent', () => {
   let component: SystemConfigurationLinkComponent;
@@ -8,7 +10,16 @@ describe('SystemConfigurationLinkComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SystemConfigurationLinkComponent]
+      imports: [SystemConfigurationLinkComponent, HttpClientTestingModule],
+      providers: [
+        {
+          provide: DynamicDialogConfig,
+          useValue: {
+            data: {},
+          },
+        },
+        { provide: DynamicDialogRef, useValue: {} },
+      ]
     })
     .compileComponents();
     
