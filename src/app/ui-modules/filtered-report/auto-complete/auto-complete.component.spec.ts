e5f3ee4d334df714730e4c55476e0c37d572b304
Jan Moxter <jan.moxter@efaps.org>
2026-01-16 14:27:13 -0500

    Correct test

--- src/app/ui-modules/filtered-report/auto-complete/auto-complete.component.spec.ts
+++ src/app/ui-modules/filtered-report/auto-complete/auto-complete.component.spec.ts
@@ -1,4 +1,11 @@
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup } from '@angular/forms';
 
 import { AutoCompleteComponent } from './auto-complete.component';
 
@@ -9,9 +16,34 @@ describe('AutoCompleteComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AutoCompleteComponent],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AutoCompleteComponent);
+    fixture.componentRef.setInput('uimodule', {
+      id: '123-456-456-465',
+      key: 'filtered-report',
+      targetMode: 'VIEW',
+      properties: {},
+      header: 'A header',
+    });
+    fixture.componentRef.setInput(
+      'formGroup',
+      new FormGroup({
+        fieldName: new FormControl<string | undefined>(undefined),
+      }),
+    );
+
+    fixture.componentRef.setInput('formItem', {
+      type: 'AUTOCOMPLETE',
+      label: 'label',
+      name: 'fieldName',
+    });
+
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
