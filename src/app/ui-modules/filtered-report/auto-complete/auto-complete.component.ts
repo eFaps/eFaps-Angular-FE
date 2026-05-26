7505f9ec6f4a89f3ce33c6323175c727cf0ddb83
Jan Moxter <jan.moxter@efaps.org>
2026-05-26 17:15:16 -0500

    Correct autocomplete for filtered report

--- src/app/ui-modules/filtered-report/auto-complete/auto-complete.component.ts
+++ src/app/ui-modules/filtered-report/auto-complete/auto-complete.component.ts
@@ -25,19 +25,25 @@ export class AutoCompleteComponent {
 
   multiple = false;
   autoCompleteSuggestions = signal<Option[]>([]);
-  selectedOptions = model<Option[]>([]);
+  selectedOptions = model<Option[] | Option>([]);
 
   constructor() {
     effect(() => {
       var item = this.formItem();
-      if (item && item.options && item.value) {
-        this.selectedOptions.set(item.options);
-      }
       if (item && item.config) {
         this.multiple = item.config.multiple;
       }
+
+      if (item && item.options && item.value) {
+        if (this.multiple) {
+          this.selectedOptions.set(item.options);
+        } else {
+          this.selectedOptions.set(item.options[0]);
+        }
+      }
     });
   }
+  
   search(query: string) {
     const url = `${this.utilService.evalApiUrl()}/ui/modules/filtered-report/${this.uimodule().id}/autocomplete/${this.formItem()!!.name}`;
     const values: any = {};
