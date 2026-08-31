81a53f59a0681fd7a19179b34b71a5a9ef09e9bf
Jan Moxter <jan.moxter@efaps.org>
2026-08-30 22:52:25 -0500

    Correct test

--- src/app/content/modal-content/modal-content.component.spec.ts
+++ src/app/content/modal-content/modal-content.component.spec.ts
@@ -36,6 +36,9 @@ describe('ModalContentComponent', () => {
               outline: {
                 header: {},
               },
+              item: {
+                id: "some id"
+              }
             },
           },
         },
